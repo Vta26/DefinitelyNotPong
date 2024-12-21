6b742d9b21119478f5c7356f45c482930815e28b
Vincent Tang <Vincechtang@gmail.com>
2024-12-20 16:52:04 -0800

    Ball actually bounces now

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -9,7 +9,7 @@ public class BallScript : MonoBehaviour
     public int y_value = 1;
     void Start()
     {
-        
+        print("Game Start");
     }
 
     // Update is called once per frame
@@ -19,32 +19,29 @@ public class BallScript : MonoBehaviour
         transform.position += move * _speed * Time.deltaTime;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.tag == "KillZone")
+        {
+            x_value = 0;
+            y_value = 0;
+            transform.position = new Vector3(0,0,0);
+            //if enter is pressed, randomize between y = 1 and y = -1, and set x to whichever side got scored on
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collision Detected");
         if (collision.gameObject.tag == "Player")
         {
             print("Player Collision");
-            if (x_value == -1)
-            {
-                x_value = 1;
-            }
-            else
-            {
-                x_value = -1;
-            }
+            x_value = -x_value;
         }
 
         if (collision.gameObject.tag == "Wall")
         {
             print("Wall Collision");
-            if (y_value == -1)
-            {
-                y_value = 1;
-            }
-            else
-            {
-                y_value = -1;
-            }
+            y_value = -y_value;
         }
     }
 }
